691a02cf7e7aa77234a952a7211191222c503095
amantiwari9413 <tiwariaman9413@gmail.com>
2025-01-01 23:15:38 +0530

    cors add

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ import express from "express";
 import itemRouter from "./routes/itemRoute.js";
 import menuRouter from "./routes/menuRoute.js";
 import oderRouter from "./routes/orderRoute.js";
+import cors from "cors"
 const app= express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
