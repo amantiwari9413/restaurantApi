3ee6db61651f424876fffc556a18fc3fd58f8bb9
amantiwari9413 <tiwariaman9413@gmail.com>
2025-01-05 01:22:26 +0530

    image error fix-3

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,8 +1,18 @@
 import multer from "multer"
 import path from 'path';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the file URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the current directory
+const __dirname = dirname(__filename);
+// Get the parent directory
+const parentDir = resolve(__dirname, '..');
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, '/upload'); // Save to the 'upload' folder
+      cb(null, `${parentDir}/upload`); // Save to the 'upload' folder
     },
     filename: (req, file, cb) => {
       // Extract the original file extension
